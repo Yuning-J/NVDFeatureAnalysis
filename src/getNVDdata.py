ab613e2da51a05d3812bd1f6039aa54f5e8e72b9
Yuning Jiang <yuning.jiang17@gmail.com>
2022-01-21 15:48:56 +0100

    Update and rename updateDB.py to getNVDdata.py

--- src/getNVDdata.py
+++ src/getNVDdata.py
@@ -1,9 +1,10 @@
 # Software is free software released under the "GNU General Public License v3.0"
-# Copyright (c) 2021 Yuning-Jiang - yuning.jiang17@gmail.com
+# Copyright (c) 2022 Yuning-Jiang - yuning.jiang17@gmail.com
 
 import re
 import requests
 import zipfile
+import config
 from os import listdir
 from os.path import isfile, join
 
@@ -13,17 +14,17 @@ def get_nvd_data():
     for filename in re.findall("nvdcve-1.1-[0-9]*\.json\.zip",r.text):
         print(filename)
         r_file = requests.get("https://nvd.nist.gov/feeds/json/cve/1.1/" + filename, stream=True)
-        with open("zip/" + filename, 'wb') as f:
+        with open(config.nvd_zip_path + filename, 'wb') as f:
             for chunk in r_file:
                 f.write(chunk)
 
 #Extract the JSON files from .zip files.
 def unzip_data():
-    files = [f for f in listdir("zip/") if isfile(join("zip/", f))]
+    files = [f for f in listdir(config.nvd_zip_path) if isfile(join(config.nvd_zip_path, f))]
     files.sort()
     for file in files:
         print("Opening: " + file)
-        archive = zipfile.ZipFile(join("zip/", file), 'r')
+        archive = zipfile.ZipFile(join(config.nvd_zip_path, file), 'r')
         with archive as f:
             f.extractall('json')
 
